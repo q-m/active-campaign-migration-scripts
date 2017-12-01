5dec3c3c590d20df3d544417c091d588ba294f66
Sjoerd Andringa <sjoerd.andringa@me.com>
2017-12-01 14:28:32 +0100

    Fixups

--- bin/import_notes_and_emails.rb
+++ bin/import_notes_and_emails.rb
@@ -6,8 +6,6 @@ require 'active_campaign'
 require_relative '../lib/contact'
 require_relative '../lib/ac_id_lookup'
 
-require 'pp'
-
 client = ::ActiveCampaign::Client.new(api_endpoint: ENV["AC_API_URL"], api_key: ENV["AC_API_KEY"])
 lookup = AcIdLookup.new(client)
 
@@ -19,6 +17,6 @@ contact_ac_id     = lookup.by_email(contact.email)
 
 contact.notes.each do |note|
   client.contact_note_add(id: contact_ac_id, note: note.to_s)
-  sleep(0.5) # if we're going to fast we might loose its order
+  sleep(0.5) # if we're going to0 fast we might loose the order
 end
 print "Imported #{contact.notes.size} notes and emails for #{contact.email}\n"
