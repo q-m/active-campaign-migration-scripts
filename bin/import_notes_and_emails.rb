9277b2e37c8a6a0aa677fc0c55d85dbc39ef5d4f
wvengen <willem@thequestionmark.org>
2017-12-11 16:27:43 +0100

    Better error reporting

--- bin/import_notes_and_emails.rb
+++ bin/import_notes_and_emails.rb
@@ -10,20 +10,26 @@ lookup = AcIdLookup.new(client)
 
 # TODO for each txt file in /data folder
 Dir["./data/yaml/*.txt"].each do |file|
-  test_contact_data = YAML.load_file(file)
-  contact           = Contact.new(test_contact_data)
+  begin
+    s = File.read(file)
+    s.gsub!(/(Body: \|6\+\r\n      )\s*/, '\\1\\2')
+    test_contact_data = YAML.load(s)
+    contact           = Contact.new(test_contact_data)
 
-  if contact_ac_id = lookup.by_highrise_id(contact.id)
-    if contact.notes.size > 0 # avoid doing this twice
-      contact.notes.each do |note|
-        client.contact_note_add(id: contact_ac_id, note: note.to_s)
-        sleep(0.5) # if we're going too fast we might loose the order
+    if contact_ac_id = lookup.by_highrise_id(contact.id)
+      if contact.notes.size > 0 # avoid doing this twice
+        contact.notes.each do |note|
+          client.contact_note_add(id: contact_ac_id, note: note.to_s)
+          sleep(0.5) # if we're going too fast we might loose the order
+        end
+        print "[ok]   Imported #{contact.notes.size} notes and emails for contact with ActiveCampaign ID #{contact_ac_id}\n"
+      else
+        print "[ok]   Skipped ActiveCampaign ID #{contact_ac_id} because contact already has notes\n"
       end
-      print "[ok]   Imported #{contact.notes.size} notes and emails for contact with ActiveCampaign ID #{contact_ac_id}\n"
     else
-      print "[ok]   Skipped ActiveCampaign ID #{contact_ac_id} because contact already has notes\n"
+      print "[fail] Unable to find Active Campaign contact for Highrise ID #{contact.id}. Did you populate the Highrise ID field in ActiveCampaign?\n"
     end
-  else
-    print "[fail] Unable to find Active Campaign contact for Highrise ID #{contact.id}. Did you populate the Highrise ID field in ActiveCampaign?\n"
+  rescue Psych::SyntaxError => e
+    print "[fail] Parse error in '#{file}': #{e}\n"
   end
 end
